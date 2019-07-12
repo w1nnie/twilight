8c8d38a744a82d55e25af92d338913bff4e7b437
winnie <winnie.trap@gmail.com>
2019-07-12 20:48:12 +0900

    make counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div>{{ countMessage }}</div>
+        <button @click="count++">クリック!</button>
+        <button @click="count = 0">リセット!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "counter",
+    data() {
+        return {
+            count: 0
+        };
+    },
+    computed: {
+        countMessage() {
+            return "回数: " + this.count;
+        }
+    }
+};
+</script>
+
+<style></style>
