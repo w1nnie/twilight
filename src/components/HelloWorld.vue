162c7e2f2bd0f8790df3abad00328045615b0793
winnie <winnie.trap@gmail.com>
2019-07-15 23:09:52 +0900

    import anime.js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,37 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
-    <counter />
+  <div @click.stop="next">
+    <div v-show="isFirstScene" ref="first">
+    </div>
+    <div v-show="isSecondScene" ref="second">
+    </div>
   </div>
 </template>
 
 <script>
 import Counter from "./Counter.vue";
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
+  data() {
+    isFirstScene: true,
+    isSecondScene: false,
+    firstTimeline: null,
+    secondTimeline: null
   },
-  components: {
-    Counter
+
+  mounted() {
+    this.showFirstScene();
   }
-};
+
+  methods: {
+    showFirstScene() {
+      this.isFirstScene = true;
+      this.firstTimeline = anime.timeline();
+      this.firstTimeline
+       .add({})
+       .add({})
+       ...
+    }
+  }
+}
 </script>
 
 <style></style>
