16e9ae427cf29d7e125e509b2dc70d68a5a752f5
winnie <winnie.trap@gmail.com>
2019-08-16 20:10:44 +0900

    overlap images

--- src/components/MikageDelta.vue
+++ src/components/MikageDelta.vue
@@ -1,9 +1,15 @@
-<template>
-  <div>
-    {{ message }}
-    <img class="th" src="../assets/th/th_1.png">
-    <img class="th po" src="../assets/th/th_2.png">
-  </div>
+<template lang="pug">
+  div
+    h1 {{ message }}
+    .wrap
+      img.th(src="../assets/th/th_1.png")
+      img.th.po(src="../assets/th/th_2.png")
+      img.th.po(src="../assets/th/th_3.png")
+      img.th.po(src="../assets/th/th_4.png")
+      img.th.po(src="../assets/th/th_5.png")
+      img.th.po(src="../assets/th/th_6.png")
+      img.th.po(src="../assets/th/th_7.png")
+      img.th.po.yo(src="../assets/th/th_8.png")
 </template>
 
 <script>
@@ -20,10 +26,15 @@ export default {
 </script>
 
 <style scoped lang="sass">
+.wrap
+  display: flex
+  justify-content: center
+  align-items: center
 .th
-  width: 100vw
+  height: 100vh
   &.po
-    position: relative
-    top: -100vh
-    opacity: 0.4
+    position: absolute
+    opacity: 0.1
+    &.yo
+    opacity: 1
 </style>
