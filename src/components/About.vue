15268dffefd7ec4fa65f93e78d0dac1e93d09c57
winnie <winnie.trap@gmail.com>
2020-02-22 17:51:08 +0900

    add Abouttext

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,20 +3,29 @@
     .modal
       .character
       .text
-        p 自己紹介
-    router-link.top(:to="'/'") ×
+        about-text
+    router-link.collapse(:to="'/'") ×
 </template>
 
 <script>
+import AboutText from "@/components/AboutText.vue";
+
 export default {
   name: "About",
   data() {
     return {};
+  },
+  components: {
+    AboutText
   }
 };
 </script>
 
 <style scoped lang="sass">
+$color-text: #fff9e8
+$color-box: #1d1d1d
+$color-border: #999285
+
 .about-container
   position: absolute
   width: 100vw
@@ -25,41 +34,39 @@ export default {
   display: flex
   justify-content: center
   align-items: center
-  color: #fff9e8
+  color: $color-text
 
 
   .modal
     position: absolute
     width: 70vw
     height: 70vh
-    background-color: rgba(30,30,30,0.5)
-    // border-radius: 3rem
+    background-color: rgba($color-box,0.5)
     display: flex
     align-items: center
     justify-content: space-around
     border: 1px solid #999285
 
     .character
-      width: 30%
+      width: 25%
       height: 85%
       background-color: #c77
 
     .text
-      width: 50%
+      width: 60%
       height: 85%
       background-color: (0,0,0,0.1)
 
-  .top
+  .collapse
     position: absolute
     top: 15vh
     right: 15vw
     width: 4rem
     height: 4rem
-    // border-radius: 3rem
     background-color: rgba(0,0,0,0.3)
     z-index: 1000
     font-size: 3rem
     text-align: center
     text-decoration: none
-    color: #fff9e8
+    color: $color-text
 </style>
---
+++ src/components/AboutText.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+  .about-text-container
+    .bullet
+      .matters Handle Name
+      .description うぃにおん
+
+    .bullet
+      .matters Creator Name
+      .description 白葱
+
+    .bullet
+      .matters Name
+      .description Yuma Watanabe
+
+    .bullet
+      .matters Date of Birth
+      .description 1997/05/30
+
+    .bullet
+      .matters Affiliation
+      .description Tokyo Institute of Technology
+
+    .bullet
+      .matters Likes
+      .description Drawing Illust / Pixelart 
+
+</template>
+
+<script>
+export default {
+  name: "AboutText",
+  data() {
+    return {};
+  },
+  components: {}
+};
+</script>
+
+<style scoped lang="sass">
+$color-border: #999285
+$color-text: #fff9e8
+
+
+.about-text-container
+  .bullet
+    display: flex
+    font-size: 1.5rem
+    justify-content: space-around
+    margin-bottom: 3rem
+
+    .matters
+      border-right: 2px solid $color-text
+      width: 40%
+
+    .description
+      width: 40%
+</style>
