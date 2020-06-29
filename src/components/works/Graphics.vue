09994e629ed6e5b6df5b475e8e2eed0f5a9e311e
winnie <winnie.trap@gmail.com>
2020-06-29 22:45:51 +0900

    display image

--- src/components/works/Graphics.vue
+++ src/components/works/Graphics.vue
@@ -1,20 +1,24 @@
 <template lang="pug">
 .graphics-container
-  .cambus
-    .pictures(v-for="item in items") {{item.message}}{{item.poyo}}
+  .cambus()
+    router-link.item(v-for="item in graphicsData" :to="'/works/graphics/'+item.id")
+      img.item-img(:src="item.link")
+      .light-effect
+    router-view
 </template>
 
 <script>
+import graphicsData from "@/assets/graphicsData.json";
+
 export default {
   name: "Graphics",
   data() {
     return {
-      items: [
-        { message: "Foo", poyo: "po" },
-        { message: "Bar", poyo: "yo" }
-      ]
+      graphicsData,
+      po: ["@/assets/Graphics/hina.png", "@/assets/Graphics/lize_24h@0.5x.png"]
     };
-  }
+  },
+  methods: {}
 };
 </script>
 
@@ -27,11 +31,25 @@ export default {
     display: flex
     justify-content: start
 
-    .pictures
+    .item
       width: 16vw
       height: 12vw
       background-color: #aaa
-      border: 0.5vw solid #655e53
-      box-shadow: 0 1vw 3vw rgba(0,0,0,0.25)
+      border: 0.5vw solid #5e5753
+      box-shadow: 0 1vw 2vw rgba(0,0,0,0.3)
       margin: 1rem
+
+      .item-img
+        width: 100%
+        height: 100%
+        object-fit: cover
+
+    .light-effect
+        position: absolute
+        left: 0
+        top: 0
+        width: 100%
+        height: inherit
+        background-color: rgba(217,213,177,0.1)
+        z-index: 4
 </style>
