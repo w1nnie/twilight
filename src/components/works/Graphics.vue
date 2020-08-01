f6b13543671eda173f5734dfc7ed018d027e577c
winnie <winnie.trap@gmail.com>
2020-08-01 18:39:46 +0900

    hover action

--- src/components/works/Graphics.vue
+++ src/components/works/Graphics.vue
@@ -3,7 +3,12 @@
   .graphics-tags
     .graphics-tags-item(v-for="(tag, index) in tagList" @click="enableTag(index)" :class="[activeTagFlags[index] ? activeClass : '', inactiveClass]") {{tag}}
   transition-group.graphics-item-container(name="filter")
-    router-link.item(v-for="item in filteredData" :to="'/works/graphics/'+item.id" :key="item.id")
+    router-link.item(v-for="item in filteredData" 
+     :to="'/works/graphics/'+item.id" 
+     :key="item.id" 
+     @mouseover.native="zoomIn(item.id)"
+     @mouseout.native="zoomOut()"
+     :style="[isEqual(focus, item.id) ? zoom : neutral]")
       img.item-img(:style="{objectPosition: item.position}" :src="'/twilight/img/thumbnail/'+item.filename+'.jpg'")
   router-view
 </template>
@@ -28,7 +33,15 @@ export default {
       activeTagIndex: -1,
       activeClass: "graphics-tags-item-active",
       inactiveClass: "graphics-tags-item",
-      hoverZoom: "translate3D(0,0,0)"
+      zoom: {
+        transform: "scale(1.05,1.05)",
+        boxShadow: "0 0 0.5vw rgba(0,0,0,0.3)"
+      },
+      neutral: {
+        transform: "scale(1,1)",
+        boxShadow: "0 0.5vw 0.5vw rgba(0,0,0,0.3)"
+      },
+      focus: -1
     };
   },
   computed: {
@@ -59,6 +72,16 @@ export default {
         this.activeTagFlags.fill(false);
         this.activeTagFlags[index] = true;
       }
+    },
+    zoomIn(i) {
+      this.focus = i;
+    },
+    zoomOut() {
+      this.focus = 1000;
+    },
+    isEqual(a, b) {
+      console.log(a == b);
+      return a == b;
     }
   }
 };
@@ -99,15 +122,16 @@ export default {
     overflow: scroll
 
     .item
-      width: 30vh
-      height: 30vh
+      width: 32vh
+      height: 32vh
       background-color: rgb(213,210,172)
       border: 5px solid rgb(74,67,63)
       border-top: 5px solid rgb(140,120,90)
       box-sizing: border-box
-      box-shadow: 0 1vw 0.5vw rgba(0,0,0,0.3)
-      margin: 1rem
-      transition: translate3d(0,0,0)
+      box-shadow: 0 0.5vw 0.5vw rgba(0,0,0,0.3)
+      margin: 1vh
+      transition: all .2s
+      translate-origin: left top
 
       .item-img
         width: 100%
