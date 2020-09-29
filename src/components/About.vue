1099a81d15791744114d25ec2fd3455ecdeecf15
winnie <winnie.trap@gmail.com>
2020-09-29 19:27:36 +0900

    100vh problem

--- src/components/About.vue
+++ src/components/About.vue
@@ -31,10 +31,8 @@ export default {
 
 .about-container
   position: absolute
-  top: 0
-  left: 0
-  width: 100vw
-  height: 100vh
+  width: 100%
+  height: 100%
   display: flex
   justify-content: center
   align-items: center
